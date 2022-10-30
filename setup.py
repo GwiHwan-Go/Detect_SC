91ac78978100854509603813a67c69d7565da793
GwiHwan Go <83144588+GwiHwan-Go@users.noreply.github.com>
2022-10-31 00:39:09 +0900

    upload setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Detect_SC',
+    version='0.1dev',
+    packages=['Detect_SC',],
+    license='None',
+    long_description=open('README.txt').read(),
+)
