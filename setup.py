b9f45f95af6defd0e03fb4434eacceff7e154069
GwiHwan Go <83144588+GwiHwan-Go@users.noreply.github.com>
2022-10-31 00:42:18 +0900

    update setup file

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='Detect_SC',
     version='0.1dev',
     packages=['Detect_SC',],
+    author='Gwihwan Go',
+    author_email='ie1914@g.skku.edu',
     license='None',
+    url='https://github.com/GwiHwan-Go/Detect_SC',
     long_description=open('README.txt').read(),
 )
